68eeffa0f88c5fe840568ca7e0cff8f94b25ac83
balanand003 <balanandm01@gmail.com>
2018-08-06 14:32:31 +0530

    Create OddNumber.java

---
+++ OddNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class OddNumber
+{
+public static void main(String ards[])
+{
+System.out.println("the odd numbers are:")
+for(int i=1;i<=6;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i+" ");
+}
+}
+}
+}
